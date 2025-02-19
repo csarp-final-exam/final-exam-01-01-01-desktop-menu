cf80868e2093a7a2db2f411edcf205d1b81abcf2
gycsaba <gycsaba@SZT9-24>
2025-02-19 12:22:29 +0100

    CurrentView

--- KretaProject/ViewModels/MainWindowViewModel.cs
+++ KretaProject/ViewModels/MainWindowViewModel.cs
@@ -4,5 +4,7 @@ namespace KretaProject.ViewModels
 {
     public class MainWindowViewModel : ObservableObject
     {
+        [ObservableProperty]
+        public object _currentView = new object();
     }
 }
