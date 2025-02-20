3a4ad55eafd6468fc6452b6afe648fac0c1663c6
gycsaba <gycsaba@SZT9-24>
2025-02-20 08:28:23 +0100

    ViewModelek beépítése a MainWindowViewModel

--- KretaProject/ViewModels/MainWindowViewModel.cs
+++ KretaProject/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,16 @@ namespace KretaProject.ViewModels
 {
     public partial class MainWindowViewModel : ObservableObject
     {
+        private readonly StudentViewModel _studentViewModel= new StudentViewModel();
+        private readonly SchoolClassViewModel _schoolClassViewModel = new SchoolClassViewModel();
+        private readonly ControlPanelViewModel _controlPanelViewModel = new ControlPanelViewModel();
+
         [ObservableProperty]
-        public object _currentView = new ControlPanelViewModel();
+        public object _currentView = new object();
+
+        public MainWindowViewModel()
+        {
+            _currentView = _controlPanelViewModel;
+        }
     }
 }
